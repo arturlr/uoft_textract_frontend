9787cf5ae56b361eb4e5b94a7a57f5c84645c176
Rodrigues <artrodri@a483e77799ff.ant.amazon.com>
2021-01-31 02:47:02 -0800

    adding SAM

---
+++ deploy_lambda.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [[ ! -f "./amplify/.config/project-config.json" ]]; then
+    echo 'Project file does not exist'
+    exit 1
+fi
+
+PROJECT_NAME=$(cat ./amplify/.config/project-config.json | jq -r '.projectName')
+echo "Project Name: ${PROJECT_NAME}"
+S3_BUCKET=$(aws resourcegroupstaggingapi get-resources --tag-filters Key=user:Application,Values="${PROJECT_NAME}" --resource-type-filters s3 --query 'ResourceTagMappingList[*].[ResourceARN]' --output text | grep -v deployment | awk -F':::' '{print $2}')
+echo "Bucket Name: ${S3_BUCKET}"
+DYNAMO_TABLE=$(aws resourcegroupstaggingapi get-resources --tag-filters Key=user:Application,Values="${PROJECT_NAME}" --resource-type-filters dynamodb --query 'ResourceTagMappingList[*].[ResourceARN]' --output text | cut -f2- -d/)
+echo "DynamoDb Table: ${DYNAMO_TABLE}"
+
+echo "Creating Layer"
+
+./create_package.sh
+
+sam package --s3-bucket ${S3_BUCKET} --output-template-file out.yaml
+sam deploy --template-file out.yaml --capabilities CAPABILITY_IAM --stack-name "${PROJECT_NAME}Lambda" --parameter-overrides ParameterKey=s3Bucket,ParameterValue="${S3_BUCKET}" ParameterKey=DynamoDbTable,ParameterValue="${DYNAMO_TABLE}"
+
+rm package.zip
+
+LAMBDA_ARN=$(aws cloudformation describe-stacks --stack-name "${PROJECT_NAME}Lambda" --query "Stacks[0].Outputs[?OutputKey=='PdfToCsvArn'].OutputValue" --output text)
+echo "Lambda: ${LAMBDA_ARN}"
+
+sed "s|%LambdaArn%|$LAMBDA_ARN|g" notification.json > notification.s3
+aws s3api put-bucket-notification-configuration --bucket "${S3_BUCKET}" --notification-configuration file://notification.s3 --output text
